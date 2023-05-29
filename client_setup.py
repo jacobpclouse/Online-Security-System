3a35dfe9278332721612668c2a78d130c29b03ec
Jacob Clouse <clouse.jp@gmail.com>
2023-05-29 10:56:19 -0400

    added the ability to stream data from client to server, need to add the ablity to save the stream as well as view it (indexing it in a db), have multiple cameras, etc

--- client_setup.py
+++ client_setup.py
@@ -1,40 +1,42 @@
 import cv2
 import socket
+import pickle
+import struct
+import keyboard
 
-def display_camera_feed():
-    # Create a socket object
+def main():
+    # Initialize the client socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect(('localhost', 8000))
 
-    # Define the server address and port
-    server_address = ('localhost', 12345)
-
-    # Connect to the server
-    client_socket.connect(server_address)
-    print("Connected to server.")
-
-    # Receive the notification from the server
-    message = client_socket.recv(1024).decode()
-    print("Server message:", message)
-
-    # Start reading the saved camera feed
-    cap = cv2.VideoCapture('camera_feed.avi')
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
+    # Receive the frame from the server and display it
+    data = b""
+    payload_size = struct.calcsize("Q")
+    while True:
+        while len(data) < payload_size:
+            packet = client_socket.recv(4 * 1024)  # 4KB
+            if not packet:
+                break
+            data += packet
+        if not data:
             break
+        packed_msg_size = data[:payload_size]
+        data = data[payload_size:]
+        msg_size = struct.unpack("Q", packed_msg_size)[0]
+        while len(data) < msg_size:
+            data += client_socket.recv(4 * 1024)
+        frame_data = data[:msg_size]
+        data = data[msg_size:]
+        frame = pickle.loads(frame_data)
+        cv2.imshow('Client Stream', frame)
+        cv2.waitKey(1)
 
-        # Display the frame
-        cv2.imshow('Camera Feed', frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        # Check if 'q' is pressed
+        if keyboard.is_pressed('q'):
             break
-
-    # Release resources
-    cap.release()
-    cv2.destroyAllWindows()
 
     # Close the client socket
     client_socket.close()
 
 if __name__ == '__main__':
-    display_camera_feed()
+    main()
--- server_setup.py
+++ server_setup.py
@@ -1,62 +1,44 @@
 import cv2
 import socket
 import pickle
+import struct
 
-def save_camera_feed():
-    # Open camera
-    cap = cv2.VideoCapture(0)
+def send_frame(conn, frame):
+    # Serialize the frame
+    data = pickle.dumps(frame)
+    # Pack the serialized frame and send its size
+    message = struct.pack("Q", len(data)) + data
+    # Send the frame to the client
+    conn.sendall(message)
 
-    # Define the codec and create a VideoWriter object
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('camera_feed.avi', fourcc, 20.0, (640, 480))
-
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        # Save the frame
-        out.write(frame)
-
-    # Release resources
-    cap.release()
-    out.release()
-
-def run_server():
-    host = ''  # Bind to all available interfaces
-    port = 12345
-
-    # Create a socket object
+def main():
+    # Initialize the server socket
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('0.0.0.0', 8000))
+    server_socket.listen(5)
+    print("Server started, waiting for client connection...")
 
-    # Bind the socket to a specific address and port
-    server_socket.bind((host, port))
+    # Accept a client connection
+    conn, addr = server_socket.accept()
+    print("Client connected:", addr)
 
-    # Listen for incoming connections
-    server_socket.listen(1)
-
-    print("Server is running and waiting for a client connection...")
+    # Open the camera
+    camera = cv2.VideoCapture(0)
 
     while True:
-        # Accept a client connection
-        client_socket, addr = server_socket.accept()
-        print("Client connected:", addr)
+        # Read a frame from the camera
+        _, frame = camera.read()
 
-        # Save camera feed
-        save_camera_feed()
+        # Send the frame to the client
+        send_frame(conn, frame)
 
-        # Send a notification to the client
-        message = "Camera feed saved successfully!"
-        client_socket.sendall(message.encode())
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) == ord('q'):
+            break
 
-        # Close the connection
-        client_socket.close()
-
-        # Exit the loop to only serve one client at a time
-        break
-
-    # Close the server socket
+    # Release the camera and close the server socket
+    camera.release()
     server_socket.close()
 
 if __name__ == '__main__':
-    run_server()
+    main()
